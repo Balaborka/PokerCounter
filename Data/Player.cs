0ced38077035d642aec3581bca3565818d9e2af8
Boris Balaban <BorisBalaban71@gmail.com>
2020-09-01 21:27:29 +0300

    Init Commit

---
+++ Data/Player.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Poker_Counter.Data
+{
+    public class Player
+    {
+        public string Name { get; set; }
+
+        public int Summary { get; set; }
+
+        public int Order { get; set; }
+
+    }
+}
