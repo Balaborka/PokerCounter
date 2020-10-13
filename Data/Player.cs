0cb74397e4e8cda8d0e65631f207299a5bf264cc
Boris Balaban <BorisBalaban71@gmail.com>
2020-10-13 23:02:20 +0300

    Additional refactoring

--- Data/Player.cs
+++ Data/Player.cs
@@ -5,8 +5,7 @@ namespace Poker_Counter.Data
 {
     public class Player
     {
-        public string Name { get;
-         set; }
+        public string Name { get; set; }
         public int Summary { get; set; }
         public List<PlayerRound> Rounds { get; set; }
     }
--- Data/PlayerRound.cs
+++ Data/PlayerRound.cs
@@ -5,43 +5,43 @@ namespace Poker_Counter.Data
     public class PlayerRound
     {
         public int MaxOrder { get; set; }
-        public int Score { get; set; }
+        public int Score { get; 
+        set; }
         public int CurrentSummary { get; set; }
         public Round RoundName { get; set; }
-        public int Order { get;  set; }
-        int trick;
-        public int Trick { get => trick; set {
-            trick = value;
-            CurrentSummary = curSum + Result;
-        } }
-        public int Result
+        int order = 0;
+        public int Order
         {
-            get {
+            get => order;
+            set { order = value; }
+        }
+        int trick;
+        public int Trick
+        {
+            get => trick; set
+            {
+                trick = value;
                 var res = Trick - Order;
                 if (RoundName == Round.Minimal)
                 {
                     if (res > 0)
-                        return -(res * 10);
+                        Score = -(res * 10);
                     else
-                        return 5;
+                        Score = 5;
                 }
                 if (RoundName == Round.Gold)
                 {
                     if (res > 0)
-                        return (res * 10);
+                        Score = (res * 10);
                     else
-                        return -5;
+                        Score = -5;
                 }
                 if (res < 0)
-                    return -(res * 10);
+                    Score = -(res * 10);
                 else if (res > 0)
-                    return -(res * 5);
-                return Trick * 10;
+                    Score = -(res * 5);
+                else Score = Trick * 10;
             }
         }
-        int curSum;
-        //public PlayerRound(int sum) {
-        //    curSum = sum;
-        //}
     }
 }
