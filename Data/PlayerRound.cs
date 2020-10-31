75c88520aca925553b7c0fe523facef64b276374
Boris Balaban <BorisBalaban71@gmail.com>
2020-10-31 08:16:35 +0300

    1. Remove other pages
    2. Refactoring
    3. Edit recalculating logic

--- Data/PlayerRound.cs
+++ Data/PlayerRound.cs
@@ -3,58 +3,68 @@ using System;
 namespace Poker_Counter.Data
 {
     public class PlayerRound
-    {        
+    {
         public int MaxOrder { get; set; }
         public int RoundScore { get; set; }
         public int ResultScore { get; set; }
         public Round RoundName { get; set; }
-        public int? Order { get; set; }
-        private Player player;
-        int? trick;
-
-        public PlayerRound(Player player) {
-            this.player = player;
+        int? order;
+        public int? Order
+        {
+            get => order;
+            set
+            {
+                order = value;
+                CalculateScore();
+            }
         }
+        int? trick;
         public int? Trick
         {
             get => trick;
             set
             {
                 trick = value;
-                if (Trick != null)
-                {
-                    CalculateScore();
-                    player.Recalculate();
-                }
+                CalculateScore();
             }
+        }
+        private Player player;
+
+        public PlayerRound(Player player)
+        {
+            this.player = player;
         }
 
         private void CalculateScore()
         {
-            if (RoundName == Round.Minimal)
+            if (Trick != null && Order != null)
             {
-                if (Trick > 0)
-                    RoundScore = -((int)Trick * 10);
+                if (RoundName == Round.Minimal)
+                {
+                    if (Trick > 0)
+                        RoundScore = -((int)Trick * 10);
+                    else
+                        RoundScore = 5;
+                    return;
+                }
+                if (RoundName == Round.Gold)
+                {
+                    if (Trick > 0)
+                        RoundScore = ((int)Trick * 10);
+                    else
+                        RoundScore = -5;
+                    return;
+                }
                 else
-                    RoundScore = 5;
-                return;
-            }
-            if (RoundName == Round.Gold)
-            {
-                if (Trick > 0)
-                    RoundScore = ((int)Trick * 10);
-                else
-                    RoundScore = -5;
-                return;
-            }
-            else
-            {
-                var res = (int)Trick - (int)Order;
-                if (res < 0)
-                    RoundScore = res * 10;
-                else if (res > 0)
-                    RoundScore = -(res * 5);
-                else RoundScore = (int)Trick * 10;
+                {
+                    var res = (int)Trick - (int)Order;
+                    if (res < 0)
+                        RoundScore = res * 10;
+                    else if (res > 0)
+                        RoundScore = -(res * 5);
+                    else RoundScore = (int)Trick * 10;
+                }
+                player.Recalculate();
             }
         }
     }
