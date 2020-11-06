d1c8872266c23ab1f6e33fbb5133cec2e9632b0c
Boris Balaban <BorisBalaban71@gmail.com>
2020-11-06 10:27:41 +0300

    Remove unnecessary files

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,6 @@ namespace Poker_Counter
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
